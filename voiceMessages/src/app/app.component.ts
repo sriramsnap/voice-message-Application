dfb689a47aae33fb3118d9dc33e378f65ca9b07c
SRIRAM\Sriram <sriram.vjs@gmail.com>
2017-05-05 22:14:31 +0530

    Added Sqlite plugin with register functionality - by Sriram

--- voiceMessages/src/app/app.component.ts
+++ voiceMessages/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { SplashScreen} from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -15,8 +15,10 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      splashScreen.hide();     
+        
     });
   }
+   
 }
 
